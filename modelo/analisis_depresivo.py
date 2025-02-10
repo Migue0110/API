4d7889a0e1d1bcd23e09b049968ab12074aedb10
Migue0110 <mimora80@uan.edu.co>
2025-02-10 17:44:57 -0500

    Ajuste en librerias necesarias.

--- modelo/analisis_depresivo.py
+++ modelo/analisis_depresivo.py
@@ -3,7 +3,6 @@ import joblib
 import os
 from sklearn.feature_extraction.text import CountVectorizer
 import gdown
-import os
 
 url = "https://drive.google.com/uc?id=1Vzhzqb-WqoR3l33Zvy5awwG3LAd-2WVT"
 output_path = "modelo/training/modelo_random_forest.pkl"
