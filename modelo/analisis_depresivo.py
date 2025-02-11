d564f205d1a770fcae9e7328943806a52dd19424
Migue0110 <mimora80@uan.edu.co>
2025-02-10 19:32:53 -0500

    Descargar .pkl desde drive

--- modelo/analisis_depresivo.py
+++ modelo/analisis_depresivo.py
@@ -2,12 +2,19 @@ import pandas as pd
 import joblib
 import os
 from sklearn.feature_extraction.text import CountVectorizer
+import gdown
 
 class Analizador():
-    
+        
     def __init__(self):
         self.__model = None
-    
+        self.modelo_path = "/opt/render/project/src/modelo/training/modelo_random_forest.pkl"
+
+        if not os.path.exists(self.modelo_path):
+            print("Descargando modelo...")
+            gdown.download("https://drive.google.com/uc?id=1Vzhzqb-WqoR3l33Zvy5awwG3LAd-2WVT", self.modelo_path, quiet=False)
+            print("Modelo descargado.")
+                
     def analizar(self, texto):
         modelo_path = os.path.join(os.path.dirname(__file__), 'training', 'modelo_random_forest.pkl')
         vocabulario_path = os.path.join(os.path.dirname(__file__), 'training', 'vocabulario_countvectorizer.pkl')
